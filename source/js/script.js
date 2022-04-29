9c2e2d0f0e3990d4757454f58b63652e07f4387f
Зюзюкина Екатерина <katiezuzukina@mail.ru>
2022-04-29 22:12:22 +0300

    выполняет работу над ошибками

--- source/js/script.js
+++ source/js/script.js
@@ -2,14 +2,17 @@ let navMain = document.querySelector('.main-navigation');
 let navToggle = document.querySelector('.main-header__toogle');
 
 navMain.classList.remove('main-navigation--nojs');
-// navMain.classList.add('main-navigation--closed');
+navToggle.classList.remove('main-header__toogle--nojs');
 
 navToggle.addEventListener('click', function () {
   if (navMain.classList.contains('main-navigation--closed')) {
     navMain.classList.remove('main-navigation--closed');
-    navMain.classList.add('main-navigation--opened');
+
+    navToggle.classList.remove('main-header__toogle--closed');
+    navToggle.classList.add('main-header__toogle--opened');
   } else {
     navMain.classList.add('main-navigation--closed');
-    navMain.classList.remove('main-navigation--opened');
+    navToggle.classList.remove('main-header__toogle--opened');
+    navToggle.classList.add('main-header__toogle--closed');
   }
 });
